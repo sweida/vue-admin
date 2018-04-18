75f7f0e7eb58e6feff22945b3a363c3151ad5756
[sweida] <[weida@ifm360.cn]>
2018-04-18 16:20:55 +0800

    预览

--- config/index.js
+++ config/index.js
@@ -55,12 +55,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../storage/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../storage'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
